90e4fa90fe36c90934312843458402a29b3f5451
Rahulsiddarth <43289009+rahulsiddarth@users.noreply.github.com>
2023-10-24 12:17:23 +0000

    add react-navigation

--- FoodDelivery/App.js
+++ FoodDelivery/App.js
@@ -1,20 +1,12 @@
+import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Navigation from './navigation';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text className="text-green-500">Food Delivery App !</Text>
-      <StatusBar style="auto" />
-    </View>
+   <Navigation />
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
