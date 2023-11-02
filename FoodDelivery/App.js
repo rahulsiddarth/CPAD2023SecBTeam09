2dad02246f1d2d601c43090bce92b9cc99cbb19b
nagbhushanr <119582923+nagbhushanr@users.noreply.github.com>
2023-11-02 06:04:24 +0000

    Adding redux for react

--- FoodDelivery/App.js
+++ FoodDelivery/App.js
@@ -2,10 +2,14 @@ import { NavigationContainer } from '@react-navigation/native';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import Navigation from './navigation';
+import { Provider } from 'react-redux';
+import { store } from './store';
 
 export default function App() {
   return (
-   <Navigation />
+    <Provider store={store}>
+      <Navigation />
+    </Provider>
   );
 }
 
