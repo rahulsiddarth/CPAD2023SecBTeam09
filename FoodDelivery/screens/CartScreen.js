7faf23818ee9e252fe07b6c5c7a319239cd3ca0d
Sonal Jain <73333649+sonaljain177@users.noreply.github.com>
2023-11-03 08:25:45 +0000

    redux changes in cart screen

--- FoodDelivery/screens/CartScreen.js
+++ FoodDelivery/screens/CartScreen.js
@@ -1,14 +1,32 @@
 import { View, Text, TouchableOpacity, ScrollView, Image } from 'react-native';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { themeColors } from '../theme';
 import * as Icon from "react-native-feather";
 import { useNavigation } from '@react-navigation/native';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { selectRestaurant } from '../slices/restaurantSlice';
+import { removeFromCart, selectCartItems, selectCartTotal } from '../slices/cartSlice';
 
 export default function CartScreen() {
         const restaurant = useSelector(selectRestaurant);
         const navigation = useNavigation();
+        const cartItems = useSelector(selectCartItems);
+        const cartTotal = useSelector(selectCartTotal);
+        const [groupedItems, setGroupedItems] = useState({});
+        const dispatch = useDispatch();
+        const deliveryFee = 2;
+
+        useEffect(()=>{
+            const items = cartItems.reduce((group,item)=>{
+                if(group[item.id]){
+                    group[item.id].push(item);
+                }else{
+                    group[item.id] = [item];
+                }
+                return group;
+            },{})
+            setGroupedItems(items);
+        },[cartItems])
     return(
         <View className = "bg-wh flex-1">
             {/*back button*/}
@@ -44,12 +62,13 @@ export default function CartScreen() {
                 }}
                 className = "bg-white pt-5">
                     {
-                        restaurant.dishes.map((dish, index)=>{
+                        Object.entries(groupedItems).map(([key, items])=>{
+                            let dish = items[0];
                             return (
-                                <View key={index}
+                                <View key={key}
                                 className = "flex-row items-center space-x-3 py-2 px-4 bg-white rounded-3xl mx-2 mb-3 shadow-mb">
                                      <Text className = "font-bold" style={{color:themeColors.text}}>
-                                        2 x
+                                        {items.length} x
                                      </Text>
                                      <Image className = "h-14 w-14 rounded-full" source = {dish.image}/>
                                      <Text className = "flex-1 font-bold text-gray-700">
@@ -58,7 +77,9 @@ export default function CartScreen() {
                                      <Text className = "font-semibold text-base">
                                         ${dish.price}
                                      </Text>
-                                     <TouchableOpacity className = "p-1 rounded-full" style={{backgroundColor: themeColors.bgColor(1)}}>
+                                     <TouchableOpacity className = "p-1 rounded-full" 
+                                        onPress={()=> dispatch(removeFromCart({id: dish.id}))}
+                                        style={{backgroundColor: themeColors.bgColor(1)}}>
                                         <Icon.Minus strokeWidth={2} height={20} width={20} stroke="white"/>
                                      </TouchableOpacity>
                                 </View>
@@ -70,15 +91,15 @@ export default function CartScreen() {
             <View style={{backgroundColor: themeColors.bgColor(0.2)}} className="p-6 px-8 rounded-t-3xl space-y-4">
                 <View className = "flex-row justify-between">
                     <Text className="text-gray-700">Subtotal</Text>
-                    <Text className="text-gray-700">$20</Text>
+                    <Text className="text-gray-700">${cartTotal}</Text>
                 </View>
                 <View className = "flex-row justify-between">
                     <Text className="text-gray-700">Delivery Fee</Text>
-                    <Text className="text-gray-700">$2</Text>
+                    <Text className="text-gray-700">${deliveryFee}</Text>
                 </View>
                 <View className = "flex-row justify-between">
                     <Text className="text-gray-700 font-extrabold">Order Total</Text>
-                    <Text className="text-gray-700 font-extrabold">$30</Text>
+                    <Text className="text-gray-700 font-extrabold">${deliveryFee+cartTotal}</Text>
                 </View>
                     <TouchableOpacity 
                         onPress={() => navigation.navigate('OrderPreparing')}
